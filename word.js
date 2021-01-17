0522684b53794b4a4a41eafe252b27514a89d900
Matthew Courtney <matthewdc182@gmail.com>
2021-01-16 23:26:58 -0500

    added in the word module

--- word.js
+++ word.js
@@ -4,4 +4,30 @@ class Word {
     constructor(word) {
         this.letters = word.split("").map(character => new Letter(character))
     }
+
+    getSolution() {
+        return this.letters.map(letter => letter.getSolution()).join("")
+    }
+
+    toString() {
+        return this.letters.join(" ")
+    }
+
+    guessLetter(character) {
+        let foundLetter = false;
+        this.letters.forEach(letter => {
+            if(letter.checkGuess(character)) {
+                foundLetter = true;
+            }
+        });
+
+        console.log("\n" + this + "\n");
+        return foundLetter;
+    }
+
+    correctGuess() {
+        return this.letters.every(letter => letter.visible)
+    }
 }
+
+module.exports = Word;
